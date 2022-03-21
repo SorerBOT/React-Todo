f19a2d34f39453d29b586b444f77b8bd5454ee9c
SorerBOT <alonfiller@gmail.com>
2022-03-21 17:12:22 +0200

    (Feat) button colours are responsive to Task Status. (completed / not completed)

--- src/Components/Task.js
+++ src/Components/Task.js
@@ -3,7 +3,7 @@ import image from "../Assets/icon-check.svg"
 export default function Task({ task, toggleTaskStatus }) {
     return (
         <div className="task" key={task.id} >
-            <button className="button" onClick={() => toggleTaskStatus(task)}>
+            <button style={{ backgroundImage: task.complete ? "linear-gradient(hsl(192, 100%, 67%), hsl(280, 87%, 65%))" : "none" }} className="button" onClick={() => toggleTaskStatus(task)}>
                 {task.complete && <img src={image} />}
             </button>
             <h3 className="description" style={{ textDecoration: task.complete ? "line-through" : "none" }}>{task.description}</h3>
